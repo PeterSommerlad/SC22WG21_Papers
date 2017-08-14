3bb96bd22794246a8ab51ba2c944a599c0d28e45
Peter Sommerlad <peter.sommerlad@hsr.ch>
2017-08-14 16:46:26 +0200

    add license to P0052 source code

--- workspace/P0052_scope_exit/src/unique_resource.h
+++ workspace/P0052_scope_exit/src/unique_resource.h
@@ -1,10 +1,32 @@
 #ifndef UNIQUE_RESOURCE_H_
 #define UNIQUE_RESOURCE_H_
+/*
+ * MIT License
+
+Copyright (c) 2016/2017 Eric Niebler, slightly adapted by Peter Sommerlad
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 #include <type_traits>
 #include "_scope_guard_common.h"
 namespace std{
 namespace experimental{
-// contribution by (c) Eric Niebler 2016, slightly adapted by Peter Sommerlad
 namespace detail {
 template<typename T>
 using _is_nothrow_movable =
