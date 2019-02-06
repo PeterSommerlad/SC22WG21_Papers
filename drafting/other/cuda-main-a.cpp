9843329bdc81d9219b2281eb372f5a8f65c39905
Peter Sommerlad <peter.sommerlad@hsr.ch>
2019-02-06 16:02:39 +0100

    corrections from Tobias, upload to easychair

--- drafting/other/cuda-main-a.cpp
+++ drafting/other/cuda-main-a.cpp
@@ -1,8 +1,8 @@
-   /* Declare device memory pointers */
-   float *d_A;
-   /* Allocate CUDA memory */
-   cudaMallocManaged(&d_A, SIZE * sizeof(float));
-   /* Synchronize device and host memory */
-   cudaDeviceSynchronize();
-   /* Free the CUDA memory */
-   cudaFree(d_A);
+/* Declare device memory pointers */
+float *d_A;
+/* Allocate CUDA memory */
+cudaMallocManaged(&d_A, SIZE*sizeof(float));
+/* Synchronize device and host memory */
+cudaDeviceSynchronize();
+/* Free the CUDA memory */
+cudaFree(d_A);
